4d2935d502f491d4888ff026044e01ea5d9da5fc
Srihari <158026866+srihari-2908@users.noreply.github.com>
2025-03-26 21:17:43 +0530

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,7 +4,7 @@ const app = require("../index"); // Import the Express app
 describe("GET /", () => {
   it("should return a success message", async () => {
     const res = await request(app).get("/");
-    expect(res.statusCode).toEqual(200);
+    expect(res.statusCode).toEqual(201);
     expect(res.text).toBe("Hello, Node.js App is Running! 🚀");
   });
 });
